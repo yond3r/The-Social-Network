370dd80fb7806e28a8c75dafbfc54af9440f3daa
Hope Barrow <barr.writing@gmail.com>
2022-08-23 08:39:32 -0500

    finialized ?? will debug

---
+++ server.js
@@ -0,0 +1,18 @@
+const db = require("./config/connection");
+const routes = require("./routes");
+const express = requre("express");
+
+const app = express();
+
+
+const PORT = process.env.PORT || 3000;
+
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+app.use(routes);
+
+db.once("open", () =>{
+    app.listen(PORT, ()=>{
+        console.log(`API server running on ${PORT}!!!`);
+    });
+})
